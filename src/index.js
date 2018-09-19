7208c5a9f72d90f7263d979e0155d98dc0cd3048
ravindra rawat <ravinrawat.wd@gmail.com>
2018-09-18 19:39:21 -0600

    create App.js as react component and connect it to index.js

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App.js";
+import "../node_modules/normalize.css/normalize.css";
+import "./styles/style.scss";
+ReactDOM.render(<App />, document.getElementById("root"));
